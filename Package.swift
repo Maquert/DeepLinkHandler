a52eb2a0ed38cc3d5547299cc6d4c393b01d3a41
Miguel Hernández Jaso <maquert@me.com>
2024-09-21 19:16:05 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DeepLinkHandler",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DeepLinkHandler",
+            targets: ["DeepLinkHandler"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "DeepLinkHandler"),
+        .testTarget(
+            name: "DeepLinkHandlerTests",
+            dependencies: ["DeepLinkHandler"]
+        ),
+    ]
+)
